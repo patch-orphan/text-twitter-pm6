22f5164281951d76f62683bd2fb1ee3987ba9df4
Nova Patch <patch@cpan.org>
2015-10-18 12:38:12 -0400

    extract-urls-with-indices optimization

--- lib/Text/Twitter/Extractor.pm
+++ lib/Text/Twitter/Extractor.pm
@@ -2,7 +2,16 @@ use v6;
 use Text::Twitter::Extractor::Entity;
 
 class Text::Twitter::Extractor {
+    has $.extract-url-without-protocol = True;
+
+    # extract URL references from Tweet text
     method extract-urls-with-indices (Str $text) {
+        # performance optimization: if text doesn’t contain '.' or ':' at all,
+        # text doesn't contain a URL, so we can simply return an empty list
+        return ()
+            if !$text.defined
+            || !$text.index($.extract-url-without-protocol ?? '.' !! ':');
+
         my Text::Twitter::Extractor::Entity @urls;
 
         return @urls;
