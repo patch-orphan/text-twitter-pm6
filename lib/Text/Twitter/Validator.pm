4495750f488107af6c54df5da02f8ffde0c2d7aa
Nova Patch <patch@cpan.org>
2015-10-18 20:01:45 -0400

    unify short-url-length and short-url-length-https

--- lib/Text/Twitter/Validator.pm
+++ lib/Text/Twitter/Validator.pm
@@ -4,9 +4,7 @@ use Text::Twitter::Extractor;
 # class for validating Tweet texts
 class Text::Twitter::Validator {
     constant $max-tweet-length = 140;
-
-    has Int $.short-url-length       = 23;
-    has Int $.short-url-length-https = 23;
+    constant $short-url-length = 23;
 
     has $!extractor = Text::Twitter::Extractor.new;
 
@@ -15,9 +13,7 @@ class Text::Twitter::Validator {
             $text.codes,
             $!extractor.extract-urls-with-indices($text).map: {
                 .start - .end,
-                .value ~~ m:i{ ^ 'https://' }
-                    ?? $.short-url-length-https
-                    !! $.short-url-length
+                $.short-url-length
             }
         );
     }
@@ -26,13 +22,10 @@ class Text::Twitter::Validator {
         # BOM: U+FFFE, U+FEFF
         # Special: U+FFFF
         # Direction change: U+202A..U+202E
-        return
-            so $text.defined
-            && $text.chars
+        return $text.defined
+            && $.get-tweet-length($text) ~~ 1..$max-tweet-length
             && $text !~~ m{ <[
-                \x[FFFE] \x[FEFF] \x[FFFF]
-                \x[202A]..\x[202E]
-            ]> }
-            && $.get-tweet-length($text) <= $max-tweet-length;
+                \x[FFFE] \x[FEFF] \x[FFFF] \x[202A]..\x[202E]
+            ]> };
     }
 }
